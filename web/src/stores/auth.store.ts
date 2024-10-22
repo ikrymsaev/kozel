b332b3c607f869d691e3b21c7ac88e5de8cf5b2c
Ilya Krymsaev <ilya.krymsaev@playkot.team>
2024-10-22 14:27:53 +0300

    fix error

--- web/src/stores/auth.store.ts
+++ web/src/stores/auth.store.ts
@@ -2,6 +2,16 @@ import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { IPlayer } from "../models/IPlayer";
 
+const getMockUser = (): IPlayer => {
+  const mocked = localStorage.getItem('gk_user')
+  if (mocked) return JSON.parse(mocked) as IPlayer
+
+  const user = { id: 'i423ub6234iu6b', name: 'John Doe' }
+  localStorage.setItem('gk_user', JSON.stringify(user))
+
+  return { id: 'i423ub6234iu6b', name: 'John Doe' }
+}
+
 interface State {
   user: IPlayer | null
 }
@@ -18,14 +28,3 @@ export const useAuthStore = create<TAuthStore>()(immer((set) => ({
   user: getMockUser(), //! REMOVE LATER
   setUser: (user: IPlayer | null) => set({ user }),
 })))
-
-
-const getMockUser = (): IPlayer => {
-  const mocked = localStorage.getItem('gk_user')
-  if (mocked) return JSON.parse(mocked) as IPlayer
-
-  const user = { id: 'i423ub6234iu6b', name: 'John Doe' }
-  localStorage.setItem('gk_user', JSON.stringify(user))
-
-  return { id: 'i423ub6234iu6b', name: 'John Doe' }
-}
