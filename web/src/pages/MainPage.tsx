26505a1f58cea27f9be8c91968e1cf1afa36f529
Ilya Krymsaev <ilya.krymsaev@playkot.team>
2024-10-22 23:39:11 +0300

    move watch lobbies to service

--- web/src/pages/MainPage.tsx
+++ web/src/pages/MainPage.tsx
@@ -2,41 +2,26 @@ import { useEffect, useState } from "react";
 import { lobbyService } from "../services/lobby.service";
 import { useLobbyStore } from "../stores/lobby.store";
 import { useNavigate } from "react-router-dom";
-import { ILobby } from "../models/ILobby";
 
 export const MainPage = () => {
   const navigate = useNavigate()
 
   const activeGames = useLobbyStore((state) => state.activeGames)
-  const addLobby = useLobbyStore((state) => state.addLobby)
   
-  const [loading, setLoading] = useState(false);
+  const [creating, setCreating] = useState(false);
 
   useEffect(() => {
-    lobbyService.getLobbyList()
+    const unwatch = lobbyService.watchLobbies()
+    return unwatch
   }, [])
-
-  useEffect(() => {
-    const es = new EventSource('http://localhost:8080/hub/watch_lobbies')
-    console.log("SSE opened, watching for lobbies...")
-    es.addEventListener('new_lobby', (event) => {
-      const data = JSON.parse(event.data) as ILobby
-      console.log("SSE new_lobby: ", data)
-      addLobby(data)
-    })
-
-    return () => {
-      es.close()
-    }
-  }, [addLobby])
 
   const handleNewGame = async () => {
     try {
-      setLoading(true);
+      setCreating(true);
       await lobbyService.newGame();
     }
     finally {
-      setLoading(false);
+      setCreating(false);
     }
   }
 
@@ -47,7 +32,7 @@ export const MainPage = () => {
   return (
     <div>
       <h1>Main Page</h1>
-      <button disabled={loading} onClick={handleNewGame}>new game</button>
+      <button disabled={creating} onClick={handleNewGame}>new game</button>
       <div>
         {activeGames.map(game => (
           <div key={game.id} onClick={() => enterGame(game.id)} style={{ cursor: 'pointer' }}>
--- web/src/services/lobby.service.ts
+++ web/src/services/lobby.service.ts
@@ -1,3 +1,4 @@
+import { ILobby } from "../models/ILobby"
 import { useAuthStore } from "../stores/auth.store"
 import { useLobbyStore } from "../stores/lobby.store"
 
@@ -18,6 +19,21 @@ class LobbyService {
     const res = await fetch('http://localhost:8080/hub/lobbies')
     const games = await res.json()
     useLobbyStore.setState({ activeGames: games })
+  }
+
+  public watchLobbies = () => {
+    this.getLobbyList()
+
+    const { addLobby } = useLobbyStore.getState()
+    const es = new EventSource('http://localhost:8080/hub/watch_lobbies')
+    console.log("SSE opened, watching for lobbies...")
+    es.addEventListener('new_lobby', (event) => {
+      const data = JSON.parse(event.data) as ILobby
+      console.log("SSE new_lobby: ", data)
+      addLobby(data)
+    })
+
+    return es.close
   }
 }
 
