5cf9f8fcdd36011bdaadd417819e512e465c12ba
Ilya Krymsaev <ilya.krymsaev@playkot.team>
2024-11-01 18:40:49 +0300

    include pocketbase

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"go-kozel/internal/services"
 	router "go-kozel/internal/transport/http"
+
+	"github.com/pocketbase/pocketbase"
 )
 
 func main() {
+	pb := pocketbase.New()
+	go pb.Start()
 	hub := services.NewHub()
 
 	router.InitRouter(hub)
