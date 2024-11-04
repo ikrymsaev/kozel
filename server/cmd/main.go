5522f7fc8eafed3ba5c5b082302b7091a1f6a044
Ilya Krymsaev <ilya.krymsaev@playkot.team>
2024-11-05 00:11:58 +0300

    uncomment

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"go-kozel/internal/services"
 	router "go-kozel/internal/transport/http"
+
+	"github.com/pocketbase/pocketbase"
 )
 
 func main() {
-	// pb := pocketbase.New()
-	// go pb.Start()
+	pb := pocketbase.New()
+	go pb.Start()
 	hub := services.NewHub()
 
 	router.InitRouter(hub)
